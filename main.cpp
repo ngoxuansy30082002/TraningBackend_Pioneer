f5cb0db97af37b174ddd87b6c1d3dffba5e58fd7
Ngo Xuan Sy <ngoxuansy30082002@gmail.com>
2023-04-17 18:45:58 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define bit_set(var, pos) var |= (1 << pos)
+
+uint32_t coils = 0;
+uint16_t start_add = 0x00;
+int main()
+{
+    bit_set(coils, 31);
+    cout << coils << endl;
+    return 0;
+}
